221fc30625c59a2f443f695a6baade22aa64fe03
Emre KORKMAZ <emre.korkmaz@flo.com.tr>
2023-06-13 17:28:28 +0300

    for loop

--- hello.go
+++ hello.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+var name string = "Emre"
+var surname string = "Korkmaz"
+
 func main() {
-	fmt.Println("Emre KORKMAZ")
+	// var greeting string = "Emre"
+	name := "Emre"
+	greeting := "Hello"
+	fmt.Println(sayHello(greeting, name))
+
+	// array slice
+	markalar := []string{"Adidas", "Flo"}
+
+	for index, marka := range markalar {
+		fmt.Println(index, marka)
+	}
+}
+
+func sayHello(greeting, name string) string {
+	return greeting + " " + name
 }
